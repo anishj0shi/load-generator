dc3fa8b106aa4ac02ad0639e8db81df865788b60
Anish Joshi <anish.joshi@sap.com>
2020-11-22 12:05:11 +0100

    Adding retries for http REquests

--- cmd/log_collector/main.go
+++ cmd/log_collector/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/anishj0shi/inmemorydb-service/pkg/schema"
+	"github.com/avast/retry-go"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
@@ -13,12 +15,25 @@ import (
 	"time"
 )
 
+var (
+	inMemoryServiceUrl string
+	retryOpts          = []retry.Option{
+		retry.Attempts(5),
+		retry.Delay(4 * time.Second),
+		retry.DelayType(retry.FixedDelay),
+	}
+)
+
 func main() {
-	recordCount := 0
-	inMemoryServiceUrl := flag.String("url",
+	flag.StringVar(&inMemoryServiceUrl, "url",
 		"http://localhost:8080/eventResult", "URL for InMemoryDB Service")
 	flag.Parse()
 
+	collectLog()
+}
+
+func collectLog() {
+	recordCount := 0
 	f, err := os.OpenFile(fmt.Sprintf("latencylog-%d", time.Now().Unix()),
 		os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
@@ -43,30 +58,44 @@ func main() {
 		Timeout:       40 * time.Second,
 	}
 	for {
-		var response []schema.EventResult
-		url := fmt.Sprintf(*inMemoryServiceUrl+"?top=%d&skip=%d", top, skip)
+		if recordCount > 10000 {
+			break
+		}
+		url := fmt.Sprintf(inMemoryServiceUrl+"?top=%d&skip=%d", top, skip)
 		logrus.Infof("Calling Service: %s", url)
 		request, err := http.NewRequest(http.MethodGet, url, nil)
 		if err != nil {
 			logrus.Warning(err)
 			continue
 		}
-		res, err := client.Do(request)
-		if err != nil {
-			logrus.Warning(err)
-			continue
-		}
-		logrus.Info(res.Body)
-		if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
-			logrus.Warningf("Decoding json is a problem %v", err)
-		}
-		for _, res := range response {
-			recordCount++
-			err := writeHDRData(f, res, recordCount)
+
+		err = retry.Do(func() error {
+			res, err := client.Do(request)
 			if err != nil {
-				logrus.Warningf("Writing HDR Data is a problem %v", err)
-				continue
+				logrus.Warning(err)
+				return err
 			}
+			var response []schema.EventResult
+			logrus.Info(res.Body)
+			if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+				logrus.Warningf("Decoding json is a problem %v", err)
+			}
+			if len(response) == 0 {
+				return errors.New(fmt.Sprintf("Empty Response for top = %d, skip = %d", top, skip))
+			}
+			for _, res := range response {
+				recordCount++
+				err := writeHDRData(f, res, recordCount)
+				if err != nil {
+					logrus.Warningf("Writing HDR Data is a problem %v", err)
+					continue
+				}
+			}
+			response = nil
+			return nil
+		}, retryOpts...)
+		if err != nil {
+			logrus.Warningf("Error : %v", err)
 		}
 		skip = top
 		top = top + factor
--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.15
 
 require (
 	github.com/anishj0shi/inmemorydb-service v0.0.0-20201120175056-be10f62d33d3
+	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/common v0.9.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/tsenart/vegeta/v12 v12.8.4
+	github.com/zhashkevych/scheduler v1.0.0
 	golang.org/x/sys v0.0.0-20200622214017-ed371f2e16b4 // indirect
 )
